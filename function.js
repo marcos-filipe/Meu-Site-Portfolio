9fe5e31f1222912c43f9a671875563ecab8213b2
MARCOS FILIPE <marcos.bsantos.felipe@gmail.com>
2021-12-23 18:33:48 -0300

    Alteração na marcação e estilo.

--- function.js
+++ function.js
@@ -1,6 +1,17 @@
 $('#imgM').click(function(){
-	$('#mMobile').slideToggle(1500);
+	$('#mMobile').slideDown(1000);
+	$('#imgM').css('display', 'none');
+	$('#btnClose').css('display', 'block');
+});	
+
+
+$('#btnClose').click(function(){
+	$('#mMobile').slideUp(1000);
+	$('#btnClose').css('display', 'none');
+	$('#imgM').css('display', 'block');
 });
+
+
 
 
 
